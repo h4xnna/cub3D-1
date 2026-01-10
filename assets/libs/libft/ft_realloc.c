721f4da2b0a582b757a9c4e46662eb945506c7f9
2czu <latenciesprivate@gmail.com>
2026-01-10 17:28:19 +0100

    changed libft, reorganized everything, should be ready to go

---
+++ assets/libs/libft/ft_realloc.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_realloc.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pacda-si <pacda-si@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/10 16:18:12 by pacda-si          #+#    #+#             */
+/*   Updated: 2026/01/10 16:57:35 by pacda-si         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_realloc(void *ptr, size_t new_size)
+{
+	void	*new_ptr;
+
+	if (!ptr)
+		return (malloc(new_size));
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	new_ptr = malloc(new_size);
+	if (!new_ptr)
+		return (NULL);
+	return (new_ptr);
+}
